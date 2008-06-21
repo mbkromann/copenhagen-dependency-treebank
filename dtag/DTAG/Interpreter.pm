16377d22f613b024df73e9986924dfa611799083
mbk.isv@cbs.dk <mbk.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2008-06-21 04:33:08 +0000

    Automatic_DTAG_update
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@185 f735436f-cb49-0410-a0ae-73795322737e

--- dtag/DTAG/Interpreter.pm
+++ dtag/DTAG/Interpreter.pm
@@ -1899,18 +1899,65 @@ sub cmd_edge {
 sub cmd_edges {
 	my ($self, $graph, $noder) = @_;
 
-
 	# Dependency graphs
-	if (UNIVERSAL::isa($graph, 'DTAG::Graph'))  {
-		print "view edges at node $noder\n";
+	if (UNIVERSAL::isa($graph, 'DTAG::Graph') 
+			&& defined($noder) && $noder =~ /^[0-9]+$/)  {
+		my $node = $graph->node(($noder || 0) + $graph->offset());
+		if (! $node) {
+			return error("non-existent node $noder\n");
+		} else {
+			my @edges = map {
+					($_->in() - $graph->offset()) . " "
+					. ($_->type()) . " "
+					. ($_->out() - $graph->offset()) . "\n"} 
+				@{$node->in()};
+			print join("", sort(@edges));
+		}
 	}
 
 	# Alignment graphs
-	if (UNIVERSAL::isa($graph, 'DTAG::Alignment'))  {
-		print "view edges at node $noder\n";
+	if (UNIVERSAL::isa($graph, 'DTAG::Alignment') && defined($noder) 
+			&& $noder =~ /^([a-z])?([0-9]+)$/)  {
+		my ($key, $node) = ($1, $2);
+		$key = "a" if (! $key);
+		$node += $graph->offset($key);
+		my @edges = map {
+			my $e = $graph->edge($_); 
+			($e ? 
+				$e->outkey() . 
+				print_anodes([map {$_ - $graph->offset($e->outkey())} @{$e->outArray()}]) 
+				. ($e->type() ? " " . ($e->type()) . " " : " ") .
+				$e->inkey() .
+				print_anodes([map {$_ - $graph->offset($e->inkey())} @{$e->inArray()}])
+				. "\n"
+			: "?")} 
+			@{$graph->node_edges($key, $node) || []};
+		print join("", sort(@edges));
 	}
 
 	return 1;
+}
+
+sub print_anodes {
+	my $list = shift;
+	$list = [sort(@$list)];
+	my $inrange = 0;
+	my @newlist = ();
+	for (my $i = 0; $i <= $#$list; ++$i) {
+		if ($i > 0 && $i < $#$list 
+				&& $list->[$i-1] + 1 == $list->[$i]
+				&& $list->[$i] + 1 == $list->[$i+1]) {
+			if ($newlist[$#newlist] ne "..") {
+				push @newlist, "..";
+			}
+		} else {
+			if ($i > 0 && $newlist[$#newlist] ne "..") {
+				push @newlist, "+";
+			}
+			push @newlist, $list->[$i];
+		}
+	}
+	return join("", @newlist);
 }
 
 ## ------------------------------------------------------------
@@ -6270,7 +6317,8 @@ sub do {
 
 		# Edges: edges $node
 		$success = $self->cmd_edges($graph, $1)
-			if ($cmd =~ /^\s*edges\s+(\S+)\s*$/);
+			if ($cmd =~ /^\s*edges\s+(\S+)\s*$/
+				|| $cmd =~ /^([a-z]?[0-9]+)\s*$/);
 
 		# Etypes: etypes -$type $type1 $type2 ...
 		$success = $self->cmd_etypes($graph, $1, $2)
