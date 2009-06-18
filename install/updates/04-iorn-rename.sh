4be8bc19a507af514d44bd62d13da10a1ccce4f9
mbk.isv@cbs.dk <mbk.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2009-06-18 12:28:10 +0000

    Updated update for iørn.
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@621 f735436f-cb49-0410-a0ae-73795322737e

--- install/updates/04-iorn-rename.sh
+++ install/updates/04-iorn-rename.sh
@@ -1,26 +1,30 @@
-#!/bin/bash
+#!/bin/bash -vx
 
 home=/home/cdt
 cdtdir=$home/cdt-all/trunk
 installdir=$cdtdir/install
 user=`cat $home/.cdtname`
+export LANG=en_DK.UTF-8
 
 # This update only applies to iørn
-if cat $home/.cdtname | egrep "(iørn|iorn)" ; then
+if cat $home/.cdtname | egrep -v "(iørn|iorn)" ; then
 	echo "This update only applies to Iørn's computer"
 else
+	echo "Updating Iørn's computer"
+
 	# Find all files with iørn and rename to iorn
 	cd $cdtdir
-	for f in `echo $cdtdir/todo/iørn ; find $cdtdir/ | grep iørn | grep -v svn | grep -v malt | grep tag` ; do
-		g=`echo $f | sed -e 's/iørn/iorn/g'`
-		echo svn mv $f $g
+	for f in `ls $cdtdir/todo/i*rn ; find $cdtdir/ -print | egrep '[-]i[^-]*rn' | grep -v svn | grep -v malt | grep tag` ; do
+		g=`echo $f | sed -e 's/-i[^-]*rn/-iorn/g'`
+		echo svn mv `ls $f` $g
+		svn mv `ls $f` $g
 	done
 
 	# Rename cdtname iørn to iorn
 	echo "iorn" > $home/.cdtname
 	
 	# Rename tasks in todo
-	cat $cdtdir/todo/iorn | sed -e 's/iørn/iorn/g' > /tmp/iorn
+	cat $cdtdir/todo/i?rn | sed -e 's/-i[^-]*rn/-iorn/g' > /tmp/iorn
 	mv /tmp/iorn $cdtdir/todo/iorn
 
 	# Instruct Iørn to restart
