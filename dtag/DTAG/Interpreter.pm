1149fdb9032f78c66ffe62fa625d0377115ac383
mbk.isv@cbs.dk <mbk.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2009-08-20 11:52:09 +0000

    Automatic_DTAG_update
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@699 f735436f-cb49-0410-a0ae-73795322737e

--- dtag/DTAG/Interpreter.pm
+++ dtag/DTAG/Interpreter.pm
@@ -5010,6 +5010,7 @@ sub cmd_print {
 	if ($file) {
 		my $ps = $graph->postscript($self) || "\n";
 		my $tmpfile = $file . ".utf8";
+		my $tmpfile2 = $file . ".final";
 		#open(PSFILE, ">:encoding(iso-8859-1)", $file . "~") 
 		#open(PSFILE, ">:utf8", $tmpfile) 
 		open(PSFILE, ">", $tmpfile) 
@@ -5017,9 +5018,11 @@ sub cmd_print {
 		print PSFILE $ps;
 		close(PSFILE);
 		my $iconv = $self->{'options'}{'iconv'} || 'cat';
-		my $cmd = $iconv . " $tmpfile > $file";
+		my $cmd = $iconv . " $tmpfile > $tmpfile2";
+		system("cp $tmpfile $tmpfile2");
 		system($cmd);
 		system("rm $tmpfile");
+		system("mv $tmpfile2 $file");
 	}
 
 	# Return
