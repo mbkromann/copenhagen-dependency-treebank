d0439844e17492bef59cd1e7e00b2bf7d88e39a4
mbk.isv@cbs.dk <mbk.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2010-05-05 13:37:05 +0000

    Automatic update by matthias: compiled CDT manual
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@983 f735436f-cb49-0410-a0ae-73795322737e

---
+++ manual/disp2scripts.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+# LaTeX headings
+my @headings = ("\\chapter", "\\section", "\\subsection", "\\subsubsection");
+
+# Read arguments
+my $dispfile = shift(@ARGV);
+my $outprefix = shift(@ARGV);
+if (! ($dispfile && $outprefix)) {
+	print "Usage: disp2script <dispfile> <outfileprefix>\n";
+	print "Create .tex and .dtag scripts from disposition file\n";
+	exit(1);
+}
+
+# Open files
+open(DISP, "<:encoding(UTF-8)", $dispfile)
+	|| die "Could not open $dispfile for reading: $!";
+open(TEX, ">:encoding(UTF-8)", "$outprefix.tex") 
+	|| die "Could not open $outprefix.tex for writing: $!";
+open(DTAG, ">:encoding(UTF-8)", "$outprefix.dtag")
+	|| die "Could not open $outprefix.dtag for writing: $!";
+open(OTEX, ">:encoding(UTF-8)", "$outprefix-overviews.tex");
+
+# Process disposition
+my $overviews = "";
+while (my $line = <DISP>) {
+	chomp($line);
+
+	if ($line !~ /^\s*$/) {
+		# Split line into indented type description and heading
+		my ($itype, $heading) = split(/\s*\:\s*/, $line);
+
+		# Split indented type description into indent and type 
+		$itype =~ /^([> ]*)([^ >].*)$/;
+		my ($istring, $type) = ($1, $2);
+		$istring =~ s/\s+//g;
+		my $indent = length($istring);
+
+		# Find first type
+		$type =~ /^([^-]*).*$/;
+		my $type1 = $1 ? ": $1" : "";
+
+		# Create TEX file name
+		my $file = $outprefix . "-" . $type . ".tex";
+		my $overviewfile = $outprefix . "-" . $type . "-overview.tex";
+
+		# TeX output
+		print TEX 
+			$headings[$indent] . "{" . $heading . $type1 . "}\n\n"
+			. "\t\\input{" . $file . "}\n\n";
+		print OTEX
+			"\\overviewfile{$overviewfile}\n\n";
+
+		# DTAG output
+		print DTAG "relset2latex -file=" . $file . " " . $type . "\n";
+
+		# Debugging output
+		#print "indent=$indent type=$type heading=$heading\n";
+	}
+}
+
+close(OTEX);
+close(TEX);
+close(DTAG);
