6acff9e774afe74e9d0da5d09d1ad25acb49dee8
mbk.isv@cbs.dk <mbk.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2010-06-09 03:32:07 +0000

    Automatic update by matthias: compiled CDT manual
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@1125 f735436f-cb49-0410-a0ae-73795322737e

--- manual/confusion-table.sh
+++ manual/confusion-table.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 name=$1
-echo "\\section{Confusion table: $name}"
-echo "\\begin{tabular}{lllp{80mm}}"
-echo "\\textbf{R} & \\textbf{A} & \\textbf{N} & \\textbf{Confusion list} \\\\ \\hline"
+tmp=/tmp/confusion-table.out.$$
+
+echo "\\section{Confusion table: $name}" > $tmp
+echo "\\begin{longtable}{lllp{80mm}}" >> $tmp
+echo "\\textbf{R} & \\textbf{A} & \\textbf{N} & \\textbf{Confusion list} \\\\ \\hline" >> $tmp
 sed -e 's/^\(\([^\t]*\)\t.*\t\([0-9]*\)%=\2\)/\3\\%\t\1/g' \
 		-e 's/^\([^0-9]\)/0\\%\t\1/g' \
 	| sort -nr | sed \
 		-e 's/^\([0-9\\%]*\)\t\([^\t]*\)\t\([0-9]*\)/\\rel{\2} \& \1 \& \3 \& \\small/g'\
 		-e 's/\t\([0-9]*\)%=\([^\t]*\)/ \\confuse{\1}{\\rel{\2}}/g' \
-		-e 's/$/ \\\\/g'
-echo "\\end{tabular}"
+		-e 's/$/ \\\\/g' >> $tmp
+
+total=`cat $tmp | grep '\\%' | sed -e 's/\\%//g' | awk -F'&' '{ CONVFMT = "%3d" ; sum=sum + $3 ; wsum = wsum + $2 * $3 / 100; print "\\\\hline TOTAL & " 100 * wsum / sum "\\\\% & " sum " & \\\\\\\\"}' | tail -1`
+
+echo $total >> $tmp
+echo "\\end{longtable}" >> $tmp
+cat $tmp
+rm $tmp
