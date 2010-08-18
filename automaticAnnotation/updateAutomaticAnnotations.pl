b8dd80cb93821fc40063758ec9dafa52967596c8
mwh.isv@cbs.dk <mwh.isv@cbs.dk@f735436f-cb49-0410-a0ae-73795322737e>
2010-08-18 10:07:43 +0000

    Updated automatic annotation scripts
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@1273 f735436f-cb49-0410-a0ae-73795322737e

--- automaticAnnotation/updateAutomaticAnnotations.pl
+++ automaticAnnotation/updateAutomaticAnnotations.pl
@@ -1,8 +1,14 @@
 #!/usr/bin/perl -w
 
 use strict;
+use Cwd;
+
+
 
 my $language = $ARGV[0];
+
+# If 0 all commands are only printed - not executed. Used for debugging.
+my $execute = 1;
 
 my $useSGE = 1;
 
@@ -22,67 +28,120 @@ close(SID);
 
 
 my $ID = "$sessionID-$language";
+my $cmd = "";
+
+    my $dir = getcwd;
+
+# Update
+$cmd = "cd ../$language";
+print "$cmd\n";
+if ($execute) {
+    chdir("../$language");
+}
+
+$cmd = "svn update";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
+$cmd = "cd $dir";
+print "$cmd\n";
+if ($execute) {
+    chdir($dir);
+}
 
 
 # Find files to use for training and files to be parser
-print "findFiles.pl\n";
-system("perl findFiles.pl $sessionID $language > $ID.findFiles.out 2> $ID.findFiles.err");
+$cmd = "perl findFiles.pl $sessionID $language > $ID.findFiles.out 2> $ID.findFiles.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 # Convert files to CoNNL-format
-print "convertToConll.pl\n";
-system("perl convertToConll.pl $sessionID $language > $ID.convertToConnl.out 2> $ID.convertToConnl.err");
+$cmd = "perl convertToConll.pl $sessionID $language > $ID.convertToConnl.out 2> $ID.convertToConnl.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Sentence-segmentation of files to parse
 
 # First create list of conll files to segment:
-print "createSegmenterFileLists.pl\n";
-system("perl createSegmenterFileLists.pl $sessionID $language 2> $ID.createSegmenterFileLists.err > $ID.createSegmenterFileLists.out");
+$cmd = "perl createSegmenterFileLists.pl $sessionID $language 2> $ID.createSegmenterFileLists.err > $ID.createSegmenterFileLists.out";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Then create .txt files for segmenter
-print "../automaticSentenceSegmentation/createtoSegmentFiles.pl\n";
-system("perl ../automaticSentenceSegmentation/createtoSegmentFiles.pl $ID.toParseFiles.lst.abs 2> $ID.createtoSegmentFiles.err > $ID.createtoSegmentFiles.out");
+$cmd = "perl ../automaticSentenceSegmentation/createtoSegmentFiles.pl $ID.toParseFiles.lst.abs 2> $ID.createtoSegmentFiles.err > $ID.createtoSegmentFiles.out";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Segment txt-files
-print "../automaticSentenceSegmentation/segmentFiles.pl\n";
-system("perl ../automaticSentenceSegmentation/segmentFiles.pl $ID.toParseFiles.lst.abs it 2> $ID.segmentFiles.err > $ID.segmentFiles.out");
+$cmd = "perl ../automaticSentenceSegmentation/segmentFiles.pl $ID.toParseFiles.lst.abs it 2> $ID.segmentFiles.err > $ID.segmentFiles.out";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 # Update conll-files with sentence-segmentation
-print "segmentConllFiles.pl\n";
-system("perl segmentConllFiles.pl $sessionID $language > $ID.segmentConllFiles.out 2> $ID.segmentConllFiles.err");
+$cmd = "perl segmentConllFiles.pl $sessionID $language > $ID.segmentConllFiles.out 2> $ID.segmentConllFiles.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 # Cleanup CoNNL-files (remove line-numbers from feature-column)
-print "cleanupConll.pl\n";
-system("perl cleanupConll.pl $sessionID $language > $ID.cleanupConll.out 2> $ID.cleanupConll.err");
+$cmd = "perl cleanupConll.pl $sessionID $language > $ID.cleanupConll.out 2> $ID.cleanupConll.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Join CoNNL files for training and parsing
-print "createParserFiles.pl\n";
-system("perl createParserFiles.pl $sessionID $language > $ID.createParserFiles.out 2> $ID.createParserFiles.err");
+$cmd = "perl createParserFiles.pl $sessionID $language > $ID.createParserFiles.out 2> $ID.createParserFiles.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Create scripts to train parser and parse sentences
-print " createParserScripts.pl\n";
-system("perl createParserScripts.pl $sessionID $language > $ID.createParserScripts.out 2> $ID.createParserScripts.err");
+$cmd = "perl createParserScripts.pl $sessionID $language > $ID.createParserScripts.out 2> $ID.createParserScripts.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Run training
-my $cmd = "";
+$cmd = "";
 if ($useSGE) {
     $cmd = "qsub -cwd -sync y ";
 }
 $cmd = $cmd."./R$ID.train.sh";
 print "$cmd\n";
-system($cmd);
+if ($execute) {
+    system($cmd);
+}
 
 # Run parsing
-
 $cmd = "";
 if ($useSGE) {
     $cmd = "qsub -cwd -sync y ";
 }
 $cmd = $cmd."./R$ID.parse.sh";
 print "$cmd\n";
-system($cmd);
+if ($execute) {
+    system($cmd);
+}
+
 
 # Parsing of training files
 $cmd = "";
@@ -91,47 +150,94 @@ if ($useSGE) {
 }
 $cmd = $cmd."./R$ID.parse-trainingdata.sh";
 print "$cmd\n";
-system($cmd);
+if ($execute) {
+    system($cmd);
+}
+
 
 
 
 # Create statistics
-print "createStats.pl\n";
-system("perl createStats.pl $ID.train.conll $ID.training.out.conll > $ID.stats");
+$cmd = "perl createStats.pl $ID.train.conll $ID.training.out.conll > $ID.stats";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
 
 # Prune output from parser
-print "prune.pl\n";
-system("perl prune.pl $ID.stats $ID.out.conll $pruneAccuracy > $ID.out.conll.pruned");
+$cmd = "perl prune.pl $ID.stats $ID.out.conll $pruneAccuracy > $ID.out.conll.pruned";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 
 # Split parsed file into original texts
-print "splitParsedFiles.pl\n";
-system("perl splitParsedFiles.pl $sessionID $language > $ID.splitParsedFiles.out 2> $ID.splitParsedFiles.err");
+$cmd = "perl splitParsedFiles.pl $sessionID $language > $ID.splitParsedFiles.out 2> $ID.splitParsedFiles.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
 
 # Put result of parsing into conll-files with line numbers
-print "mergeAll.pl\n";
-system("perl mergeAll.pl $sessionID $language > $ID.mergeAll.out 2> $ID.mergeAll.err");
+$cmd = "perl mergeAll.pl $sessionID $language > $ID.mergeAll.out 2> $ID.mergeAll.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
+
 
 # Merge conll-files with tag-files
-print "mergeAllTag.pl.pl\n";
-system("perl mergeAllTag.pl $sessionID $language > $ID.mergeAllTag.out 2> $ID.mergeAllTag.err");
+$cmd = "perl mergeAllTag.pl $sessionID $language > $ID.mergeAllTag.out 2> $ID.mergeAllTag.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Update out-feature in tag-files
-print "updateAllTag.pl\n";
-system("perl updateAllTag.pl $sessionID $language > $ID.updateAllTag.out 2> $ID.updateAllTag.err");
+$cmd = "perl updateAllTag.pl $sessionID $language > $ID.updateAllTag.out 2> $ID.updateAllTag.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 # Copy tag-files to original folder
-# print "copyTagFiles.pl.pl\n";
-system("perl copyTagFiles.pl $sessionID $language > $ID.copyTagFiles.out 2> $ID.copyTagFiles.err");
+$cmd = "perl copyTagFiles.pl $sessionID $language > $ID.copyTagFiles.out 2> $ID.copyTagFiles.err";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 # Update svn
 
-system("cd ../$language");
-system("svn update");
-system("svn add *auto*.tag");
-system("svn commit -m 'Automatic annotations, $ID'");
+$cmd = "cd ../$language";
+print "$cmd\n";
+if ($execute) {
+    chdir("../$language");
+}
+
+$cmd = "svn update";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
+$cmd = "svn add *auto*.tag";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
+
+$cmd = "svn commit -m 'Automatic annotations, $ID'";
+print "$cmd\n";
+if ($execute) {
+    system($cmd);
+}
 
 
 # 
