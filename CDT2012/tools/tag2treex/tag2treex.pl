e483f156440a6b02e67474fcd95774cb5ffa2df4
zdenek.zabokrtsky@gmail.com <zdenek.zabokrtsky@gmail.com@f735436f-cb49-0410-a0ae-73795322737e>
2012-03-08 15:02:03 +0000

    creating treex nodes from tag nodes, so far all nodes in one bundle (flat trees), one zone per language,
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@1902 f735436f-cb49-0410-a0ae-73795322737e

--- CDT2012/tools/tag2treex/tag2treex.pl
+++ CDT2012/tools/tag2treex/tag2treex.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use XML::Twig;
-#use Treex::Core;
+use Treex::Core;
 
 if ( @ARGV==0 ) {
     die "Usage: $0 <list_of_file_to_be_converted_to_treex>";
@@ -27,16 +27,35 @@ foreach my $file (@ARGV) {
 foreach my $number (sort keys %files) {
     print $number,"\t",join(" ",sort keys %{$files{$number}}),"\n";
 
-    foreach my $language (sort keys %{$files{$number}}) {
+    my $doc = Treex::Core::Document->new();
+    my $bundle = $doc->create_bundle();
 
+    foreach my $language (sort keys %{$files{$number}}) {
         my $tag_document=XML::Twig->new();    # create the twig
         $tag_document->parsefile( $files{$number}{$language});
         my @sentences = $tag_document->descendants('s');
         print "  $language ".scalar(@sentences)."\n";
 
+        my $zone = $bundle->create_zone($language);
+        my $atree = $zone->create_atree;
+        my $ord = 0;
+        foreach my $tag_token ($tag_document->descendants('W')) {
+            $ord++;
+            my $anode = $atree->create_child(
+                {
+                    form => $tag_token->text,
+                    ord => $ord,
+                }
+            );
+            foreach my $attr_name (keys %{$tag_token->{'att'}||{}}) {
+                $anode->wild->{$attr_name} = $tag_token->{'att'}->{$attr_name};
+            }
+        }
     }
 
+    $doc->save('test.treex.gz');
 }
+
 
 
 
