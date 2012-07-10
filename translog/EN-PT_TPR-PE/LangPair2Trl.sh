6704f519a62fb16a1f236fee1db2cc8bafc42ea4
m.gummiball <m.gummiball@f735436f-cb49-0410-a0ae-73795322737e>
2012-07-10 15:40:12 +0000

    delete old folder
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@2021 f735436f-cb49-0410-a0ae-73795322737e

--- translog/EN-PT_TPR-PE/LangPair2Trl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in Translog-II/*.xml ; do
-   echo "Inserting -s en -t pt into $file ";
-   ../LangPair2Trl.pl -T $file -s en -t pt > $file.langPair
-   mv $file.langPair $file
-done
