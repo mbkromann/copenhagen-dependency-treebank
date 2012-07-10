6b75d264065a56f90ab598fea03f14baf1acd66a
m.gummiball <m.gummiball@f735436f-cb49-0410-a0ae-73795322737e>
2012-07-10 21:27:22 +0000

    rename EN-HI into NJ12
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@2023 f735436f-cb49-0410-a0ae-73795322737e

--- translog/EN-HI/LangPair2Trl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in Translog-II/*.xml ; do
-   echo "Inserting -s en -t hi into $file ";
-   ../LangPair2Trl.pl -T $file -s en -t hi > $file.langPair
-   mv $file.langPair $file
-done
