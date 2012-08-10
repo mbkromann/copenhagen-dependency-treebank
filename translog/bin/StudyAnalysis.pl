18eb76b1d5f2da8d40388daf6a0c66828b1f66d0
abhijitmishra.530@gmail.com <abhijitmishra.530@gmail.com@f735436f-cb49-0410-a0ae-73795322737e>
2012-08-10 18:53:56 +0000

    perl
    git-svn-id: https://copenhagen-dependency-treebank.googlecode.com/svn/trunk@2071 f735436f-cb49-0410-a0ae-73795322737e
    
    Former-commit-id: da86748a6ac4589524ed96279c1e89f254f94289

--- translog/bin/StudyAnalysis.pl
+++ translog/bin/StudyAnalysis.pl
@@ -6,19 +6,19 @@ use File::Path qw(make_path remove_tree);
 use File::stat;
 
 if ($#ARGV !=1){
-	print "Usage $ARGV[0] <command:[make|clean]> <Study_name | all>\n";
+	print "Usage <command:[make|clean]> <Study_name | all>\n";
   	exit 1;
 	
 }
 
-my $STUDY = qw(ACS08 BD08 BML12 JLG10 KTHJ08 LWB09 MS12 NJ12 SG12 TPR11);
+my $study_name = qw(ACS08 BD08 BML12 JLG10 KTHJ08 LWB09 MS12 NJ12 SG12 TPR11);
 
 sub CopyData{
-	my $study =shift;
+	my $study_name =shift;
 	my @folders = ("Translog-II","Alignment");
 	
 	foreach my $folder (sort @folders){
-		my $path = "../$study/$folder/";
+		my $path = "../$study_name/$folder/";
 		
 		my $pattern = "\.xml";
 		if ($folder eq "Alignment"){$pattern = "(\.src|\.tgt|\.atag)";}
@@ -36,7 +36,7 @@ sub CopyData{
 					
 					next;	
 				}
-				my $dir = "data/$study/$folder";
+				my $dir = "data/$study_name/$folder";
 				if ( not -e $dir){
 					make_path $dir or die "Directory creation failed: $!";
 				}
@@ -52,9 +52,9 @@ sub CopyData{
 	
 }
 sub MergeEvents2Trl{
-    my $study = shift;
-    my $dir = "data/$study/Events";
-    my $trans_path = "data/$study/Translog-II";
+    my $study_name = shift;
+    my $dir = "data/$study_name/Events/";
+    my $trans_path = "data/$study_name/Translog-II/";
     if ( not -e $dir){
        make_path $dir or die "Directory creation failed: $!";
     }
@@ -62,29 +62,203 @@ sub MergeEvents2Trl{
     my @FILES= readdir(DIR);
     foreach my $file (@FILES){
     	if ($file=~ m/\.xml$/i){
-           my $outp ="";
-           my $atag ="";
-           if ( older_than($file,)){
-
+    		my $temp_path = $trans_path.$file;
+    		my $log_path = $temp_path;
+    		$temp_path =~ s/\.xml$//;
+           my $outp = $temp_path;
+           $outp =~ s/Translog-II/Events/i;
+           my $atag = $temp_path;
+           $atag =~ s/Translog-II/Alignment_NLP/;
+           if ( older_than($log_path,$outp.".Event.xml") 
+           		and older_than($atag.".atag",$outp.".Event.xml")
+           		and older_than($atag.".src",$outp.".Event.xml")
+           		and older_than($atag.".tgt",$outp.".Event.xml")
+           		)
+           {
+				next;
            }
+           
+            print "MergeAtag2Trl -T $log_path -A $atag -O $outp.Atag.xml\n";
+            execute("perl ./MergeAtagTrl.pl -T $log_path -A $atag -O $outp.Atag.xml");
+            
+            print "FixMod2Trl -T $outp.Atag.xml -O $outp.Event.xml\n";
+            execute("perl ./FixMod2Trl.pl -T $outp.Atag.xml -O $outp.Event.xml");
+            execute("rm -f $outp.Atag.xml");
+            print "\n";
         }
         
-
     }
+    closedir(DIR);
+}
+sub Trl2TokenTables{
+	my $study_name = shift;
+	my $dir = "data/$study_name/Tables/";
+	my $event_path = "data/$study_name/Events/";
+	
+	if ( not -e $dir){
+       make_path $dir or die "Directory creation failed: $!";
+    }
+    opendir(DIR,$event_path);
+    my @FILES= readdir(DIR);
+    foreach my $file (@FILES){
+    	
+    	if ($file=~ m/\.Event\.xml$/i){
+	    	my $temp_path = $event_path.$file;
+	    	my $table_path = $temp_path; 
+	    	$table_path =~ s/\.Event.xml$//;
+	    	$table_path =~ s/Events/Tables/;
+	    	if (older_than($temp_path,$table_path.".st")){
+	    		next;
+	    	}
+	    	print "Token Tables -T $temp_path -O $table_path.{st,tt,fd,kd,pu,fu,au}\n"; 
+	    	execute("perl ./Trl2ProgGraphTables.pl -T $temp_path -O $table_path");
+	    	execute("perl ./Trl2TargetTokenTables.pl -T $temp_path > $table_path.tt");
+	    	execute("perl ./Trl2TargetAUTables.pl -T $temp_path > $table_path.au");
+	    }
+    }
+   closedir(DIR); 
+}
+sub ToSingleTreex{
+	my $study_name = shift;
+	my $dir = "data/$study_name/Treex/raw/";
+	my $event_path = "data/$study_name/Events/";
+	if ( not -e $dir){
+       make_path $dir or die "Directory creation failed: $!";
+    }
+    opendir(DIR,$event_path);
+    my @FILES= readdir(DIR);
+    foreach my $file (@FILES){
+    	if ($file=~ m/\.Event\.xml$/i){
+    		my $temp_path = $event_path.$file;
+    		$file =~ s/\.Event.xml$//;
+    		my $new = $dir.$study_name."-".$file;
+    		if (older_than($temp_path,$new.".treex.gz")){
+    			next;
+    		}
+    		print "perl ./Trl2Treex.pl  -T $temp_path -O $new";
+    		execute("perl ./Trl2Treex.pl -T $temp_path -O $new");
+    	}
+    }
+    my $final_treex = "treex \\
+    Misc::Translog::BuildTreesFromOffsetIndices \\
+    Util::Eval document=\"\\\$doc->set_path(qw(data/$study_name/Treex))\" Write::Treex clobber=1 storable=0 \\
+    -- data/$study_name/Treex/raw/*.treex.gz";
+    #print $final_treex;
+    execute($final_treex);
+	closedir(DIR); 
+}
+sub FinalTreex{
+	my $study_name = shift;
+	my $final_treex = "treex \\
+    Misc::Translog::BuildTreesFromOffsetIndices \\
+    Util::Eval document=\"\\\$doc->set_path(qw(data/Treex))\" Write::Treex clobber=1 storable=0 \\
+    -- data/Treex/raw/*.treex.gz";
+	execute($final_treex);
+}
+sub AnnotateTrl{
+	my $study_name = shift;
+	my $dir = "data/$study_name/Alignment_NLP/";
+	my $align_path = "data/$study_name/Alignment/";
+	my $python_args = "";
+	my $flag = 0;
+	if ( not -e $dir){
+       make_path $dir or die "Directory creation failed: $!";
+    }
+    opendir(DIR,$align_path);
+    my @FILES= readdir(DIR);
+    foreach my $file (@FILES){
+		
+    	my $temp_path = $align_path.$file;
+    	my $new_path = $temp_path;
+    	$new_path =~ s/Alignment/Alignment_NLP/;
+    	
+    	if ($temp_path=~ m/(\.src|\.tgt)$/i){
+    		
+    		if (older_than($new_path,$temp_path)){
+    			$python_args = $python_args.$temp_path." ";
+    				
+    		}
+    		
+    		
+    	}
+    	elsif($temp_path=~ m/\.atag$/i){
+    		if (older_than($new_path,$temp_path)){
+    			copy($temp_path,$new_path) or die "Copy failed: $!";
+    		}
+    	}
+    }
+    if($python_args){
+    	$python_args =~ s/^\s*(.*?)\s*$/$1/;
+    	execute("python modify_files.py $python_args");
+    }
+    closedir(DIR);
 }
 
+sub Treex2Atag{
 
+	my $study_name= shift;  
+      execute("rm -r data/$study_name/Alignment-II");
+      my $cmd = "treex \\
+      Misc::Translog::RemakeWildZones \\
+      Misc::Translog::Treex2Alignment \\
+      -- data/$study_name/Treex/*.treex.gz";
+      execute($cmd);
 
+}
 
-
+sub CheckRound{
+	my $study_name = shift; 
+	my $old_data = "data/$1/Alignment_NLP/";
+	my $new_data = "data/$1/Alignment-II/";
+	opendir(DIR,$old_data);
+    my @FILES= readdir(DIR);
+    foreach my $file (@FILES){
+    	if ($file=~ m/\.atag$/i){
+    		my $old_path = $old_data.$file;
+    		my $new_path = $new_data.$file;
+    		$old_path =~ s/\.atag$//;
+    		$new_path =~ s/\.atag$//;
+    		print "Comparing $old_path $new_path\n";
+    		execute("perl ./CompareAtag.pl -C $old_path -A $new_path");
+    	}
+    	
+    }
+    closedir(DIR);
+}
 
 sub older_than{
 	my ($file1,$file2)=@_;
-	if (stat($file1) and stat($file2)){
-		if (stat($file1)->atime < stat($file2)->atime){
+	if (stat($file1) and not(stat($file2))){
+		#print "X";
+		return 0;	
+	}
+	elsif (stat($file2) and not(stat($file1))){
+		#print "Y";
+		return 1;	
+	}
+	elsif (stat($file1) and stat($file2)){
+		if (stat($file1)->mtime < stat($file2)->mtime){
+		#	print "Z";
 			return 1;
 		}
+		else{
+		#	print "A";
+			return 0;
+		}
 	}
+	#print "M";
 	return 0;
+	
 }
-CopyData ($ARGV[1]);
+sub execute {
+	
+	my @args = ( "bash", "-c", shift );
+ 	system(@args);
+}
+
+CopyData($ARGV[1]);
+AnnotateTrl($ARGV[1]);
+MergeEvents2Trl($ARGV[1]);
+Trl2TokenTables($ARGV[1]);
+ToSingleTreex($ARGV[1]);
+Treex2Atag($ARGV[1]);
